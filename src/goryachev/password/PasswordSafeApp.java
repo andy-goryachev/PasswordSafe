0dfc3817481cda9ee44c8313d410c12e6c93f883
andy <andy@goryachev.com>
2016-10-04 18:55:59 -0700

    .

--- src/goryachev/password/PasswordSafeApp.java
+++ src/goryachev/password/PasswordSafeApp.java
@@ -25,7 +25,7 @@ import javax.swing.ImageIcon;
 public class PasswordSafeApp
 	extends Application
 {
-	public static final String VERSION   = "3.03.46";
+	public static final String VERSION   = "3.03.46+";
 	public static final String COPYRIGHT = "copyright © 2016 andy goryachev";	
 	public static final String TITLE = "PasswørdSafe";
 	public static final String EXTENSION = ".safe";
