ee628e668f49c2ebde6417acb7a72fed18da865e
andy goryachev <andy@goryachev.com>
2022-01-20 15:03:00 -0800

    delayed action swing

---
+++ src/goryachev/swing/DelayedActionSwing.java
@@ -0,0 +1,60 @@
+// Copyright © 2007-2022 Andy Goryachev <andy@goryachev.com>
+package goryachev.swing;
+import goryachev.common.log.Log;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
+
+
+// Swing delayed action
+public class DelayedActionSwing
+	extends Timer
+{
+	protected static final Log log = Log.get("DelayedActionSwing");
+
+
+	public DelayedActionSwing(int delay, Runnable callback)
+	{
+		super(delay, new ActionListener()
+		{
+			public void actionPerformed(ActionEvent ev)
+			{
+				try
+				{
+					callback.run();
+				}
+				catch(Exception e)
+				{
+					log.error(e);
+				}
+			}
+		});
+		
+		setRepeats(false);
+	}
+	
+	
+	public void action()
+	{
+		fireActionPerformed(null);
+	}
+	
+
+	public void fire()
+	{
+		stop();
+		start();
+	}
+	
+	
+	public void trigger()
+	{
+		fire();
+	}
+	
+	
+	public void cancel()
+	{
+		stop();
+	}
+}
